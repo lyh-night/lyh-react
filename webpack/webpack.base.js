71da6bfbff7df70edb8e7d3435afac3a4c149367
lyh <lyh@example.com>
2024-12-30 10:42:18 +0800

    feat(base): 项目初始化

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDev = process.env.NODE_ENV === 'development' // 是否是开发模式
 
 console.log('NODE_ENV', process.env.NODE_ENV)
 console.log('BASE_ENV', process.env.BASE_ENV)
@@ -8,7 +10,7 @@ console.log('BASE_ENV', process.env.BASE_ENV)
 module.exports = {
   entry: path.join(__dirname, '../src/index.tsx'),
   output: {
-    filename: 'static/js/[name].js', // 每个输出js的名称
+    filename: 'static/js/[name].[chunkhash:8].js', // 每个输出js的名称
     path: path.join(__dirname, '../dist'), // 打包结果输出路径
     clean: true, // webpack4 需要配置 clean-webpack-plugin 来删除 dist文件，webpack5内置了
     publicPath: '/' // 打包后文件的公共前缀路径
@@ -33,7 +35,7 @@ module.exports = {
         test: /.\css$/,
         include: [path.resolve(__dirname, '../src')],
         use: [
-          'style-loader',
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader'
         ]
@@ -42,7 +44,7 @@ module.exports = {
         test: /.\less$/,
         include: [path.resolve(__dirname, '../src')],
         use: [
-          'style-loader',
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
           'less-loader'
@@ -57,7 +59,7 @@ module.exports = {
           }
         },
         generator: {
-          filename: 'static/images/[name][ext]' // 文件输出目录和命名
+          filename: 'static/images/[name].[chunkhash:8][ext]' // 文件输出目录和命名
         }
       },
       {
@@ -69,7 +71,7 @@ module.exports = {
           }
         },
         generator: {
-          filename: 'static/fonts/[name][ext]', // 文件输出目录和命名
+          filename: 'static/fonts/[name].[chunkhash:8][ext]', // 文件输出目录和命名
         },
       },
       {
@@ -81,7 +83,7 @@ module.exports = {
           }
         },
         generator: {
-          filename: 'static/media/[name][ext]', // 文件输出目录和命名
+          filename: 'static/media/[name].[chunkhash:8][ext]', // 文件输出目录和命名
         },
       }
     ]
